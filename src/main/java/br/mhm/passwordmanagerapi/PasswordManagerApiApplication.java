477df6a3073bbeb8c26f16f8ebee48ac7f904ffc
Matheus Holanda Matos <matheus.holandam@gmail.com>
2021-10-10 15:43:21 -0300

    Initializing project

---
+++ src/main/java/br/mhm/passwordmanagerapi/PasswordManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.mhm.passwordmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordManagerApiApplication.class, args);
+	}
+
+}
